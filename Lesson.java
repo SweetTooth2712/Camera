f6f06ce8689a1c48bfcbd1c9b134ac845188bba6
SweetTooth2712 <SweetTooth-2712@yandex.ru>
2025-04-12 15:04:38 +0300

    Lesson.java

--- Lesson.java
+++ Lesson.java
@@ -5,37 +5,37 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-public class Lesson extends OpenCvPipeline {
+public class Lesson extends OpenCvPipeline { // Наследуем класс OpenCvPipeline
 
     Mat HSV = new Mat();
     Mat END = new Mat(); // Здесь мы указываем "матрицы"
     // Матрица — поток в памяти камеры. Например, Mat input — поток входного изображения, который посылается на камеру.  
 
-    Scalar HighOrange = new Scalar(25, 255, 255);
-    Scalar LowOrange = new Scalar(10, 100, 100);
+    Scalar HighColor = new Scalar(25, 255, 255); // Указываем скалярные значения для обработки цвет
+    Scalar LowColor = new Scalar(10, 100, 100);
 
-    int pixels;
+    int pixels; // Обозначаем переменную, которая будет хранить значение о количестве пикселей, входящих в вышеуказанный диапазон
 
-    Telemetry telemetry;
+    Telemetry telemetry; // Подключаем класс с телеиетрией, благодаря нему мы сможем видеть количество пикселей нужного цвета
 
-    public Lesson(Telemetry telemetry){
+    public Lesson(Telemetry telemetry) { // Прописываем конструктор 
         this.telemetry = telemetry;
     }
 
     @Override
     public Mat processFrame(Mat input) {
-        Imgproc.cvtColor(input, HSV, Imgproc.COLOR_RGB2HSV);
-        Core.inRange(HSV, LowOrange, HighOrange, END);
+        Imgproc.cvtColor(input, HSV, Imgproc.COLOR_RGB2HSV); // Эта строчка переводит входной поток в матрицу HSV, меняя с RGB в HSV
+        Core.inRange(HSV, LowColor, HighColor, END); // А здесь уже происходит вся магия. Матрица END становится черно-белой (буквально черный и белый)
 
-        for (int i = 0; i < input.width(); i++) {
+        for (int i = 0; i < input.width(); i++) { // Этот цикл позволяет проанализировать каждый пиксель матрицы END 
             for (int j = 0; j < input.height(); j++) {
                 if (END.get(j, i)[0] == 255) {
-                    pixels++;
-
+                    pixels++; // В итоге кол-во пикселей, входящих в диапазон, записывается сюда 
                 }
             }
         }
-        telemetry.addData("Pixels", pixels);
+        
+        telemetry.addData("Pixels", pixels); // Непосредтвенно вывод и последующие обнуление, чтобы анализировался каждый кадр, а не весь поток
         telemetry.update();
         pixels = 0;
 
