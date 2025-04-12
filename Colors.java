3f09659a5b87bf6c1560533d9d609f4bedef6041
SweetTooth2712 <SweetTooth-2712@yandex.ru>
2025-04-12 15:16:40 +0300

    Colors.java

--- Colors.java
+++ Colors.java
@@ -1,34 +1,23 @@
-package org.firstinspires.ftc.teamcode.pip;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-public class Mat_Detector extends OpenCvPipeline {
-    Mat hsv = new Mat();  //create hsv mat
-    Mat mask1, mask2 = new Mat();  //create masks
-    Mat end = new Mat();   //create output mat
+// Здесь указаны наиболее встречаемые цвета
 
-    String color = "green";  // create the color string
-
-    Scalar scalarLow = new Scalar(90, 100, 100);
-    Scalar scalarHigh = new Scalar(140, 255, 255);
+public class Colors extends OpenCvPipeline {
+    Mat HSV = new Mat();  
+    Mat mask1, mask2 = new Mat();  
+    Mat END = new Mat(); 
 
     Telemetry telemetry;
-
-    public Mat_Detector(Telemetry telemetry) {
-        this.telemetry = telemetry;
-    }
-
-
+    String color = "";
+    
     @Override
     public Mat processFrame(Mat input) {
-        Scalar scalarLow, scalarHigh; //create scalars for high and low values
+        Scalar scalarLow, scalarHigh;
 
         if (color == "yellow") {
             scalarLow = new Scalar(20, 100, 100);
@@ -43,18 +32,17 @@ public class Mat_Detector extends OpenCvPipeline {
             scalarLow = new Scalar(0, 0, 0);
             scalarHigh = new Scalar(0, 0, 0);
         }
-        Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);//change to hsv
+        Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
 
         if (!color.equals("red")) {
-            Core.inRange(hsv, scalarLow, scalarHigh, end);//detect color, output to end
-
-            telemetry.addLine("Color is " + color);
-            telemetry.update();
+            Core.inRange(hsv, scalarLow, scalarHigh, end);
         }
+        
         if (color == "red") {
             Core.inRange(hsv, new Scalar(0, 70, 50), new Scalar(8, 255, 255), mask1);
             Core.inRange(hsv, new Scalar(172, 70, 50), new Scalar(180, 255, 255), mask2);
-            Core.bitwise_or(mask1, mask2, end);//takes both masks and combines them
+            Core.bitwise_or(mask1, mask2, end); // Здесь присутствует функция "bitwise_or", которая позволяет объединить сразу две маски. Зачем это здесь нужно?
+            // Дело в том, что красный цвет находится как в начале столбика Hue (оттенок) в цветовой модели HSV, так и в конце. Из-за чего рекомендуется объединять эти две матрицы 
         }
         return end;
     }
