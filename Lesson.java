45c4ba49743d20714c4f536ee3fe4b67a648ab2b
SweetTooth2712 <SweetTooth-2712@yandex.ru>
2025-04-12 11:31:45 +0300

    Lesson.java

--- Lesson.java
+++ Lesson.java
@@ -1,17 +1,15 @@
-package org.firstinspires.ftc.teamcode.nonsense;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
-
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class Lesson extends OpenCvPipeline {
 
     Mat HSV = new Mat();
-    Mat END = new Mat();
+    Mat END = new Mat(); // Здесь мы указываем "матрицы"
+    // Матрица — поток в памяти камеры. Например, Mat input — поток входного изображения, который посылается на камеру.  
 
     Scalar HighOrange = new Scalar(25, 255, 255);
     Scalar LowOrange = new Scalar(10, 100, 100);
